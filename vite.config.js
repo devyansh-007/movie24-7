5f710f84ddb87989cfaa6f1e6c68a96fe2b6ba56
Your Name <you@example.com>
2025-01-27 05:58:44 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-  base: "/movieHub",
 })
 // helps to customize the build process 
 // suxch as adding plugins 
